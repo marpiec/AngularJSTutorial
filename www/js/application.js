765e4316d4a0e091a5d11a1ef849c9266846e0ed
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-09-01 22:45:08 +0200

    Services

--- www/js/application.js
+++ www/js/application.js
@@ -2,18 +2,19 @@ var app = angular.module("application", []);
 
 
 
-app.controller("HomePageController", function($scope, $http) {
+app.controller("HomePageController", function($scope, $http, booksService) {
 
     $scope.booksLoaded = false;
     $scope.books = [];
 
     $scope.loadBooks = function() {
-        $http.get("https://gist.githubusercontent.com/marpiec/90c988b3a18a96d80215/raw/c67284bc5f72b48a3812f86b11415c01642d6f32/BestJavaBooks.json").success(function(data, status, headers) {
-            $scope.books = data;
+        booksService.getBooks(function(books) {
+            $scope.books = books;
             $scope.booksLoaded = true;
-        }).error(function (data, status, headers, config) {
-            alert("Error communication with server!")
+        }, function() {
+            alert("Error communication with server!");
         });
+        booksLoaded = true;
     }
 
 });
@@ -28,9 +29,21 @@ app.directive("mpNiceBookTitle", function() {
        restrict: "E",
        template:"Title: {{ titleFormatted }} ",
        link: function(scope) {
-            alert(scope.book.title);
            scope.titleFormatted = scope.book.title.toUpperCase().replace(" ", "-");
 
        }
    }
 });
+
+
+app.factory("booksService", function($http) {
+    return {
+       getBooks: function(success, failure) {
+           $http.get("https://gist.githubusercontent.com/marpiec/90c988b3a18a96d80215/raw/c67284bc5f72b48a3812f86b11415c01642d6f32/BestJavaBooks.json").success(function(data, status, headers) {
+               success(data);
+           }).error(function (data, status, headers, config) {
+               failure();
+           });
+       }
+   }
+})
