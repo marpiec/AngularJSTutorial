4aedbc98eb2deb8f196651cfe3fa5c7f2b3debde
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2014-09-01 22:26:39 +0200

    Dependency injection and rest calls

--- www/js/application.js
+++ www/js/application.js
@@ -2,11 +2,18 @@ var app = angular.module("application", []);
 
 
 
-app.controller("HomePageController", function($scope) {
+app.controller("HomePageController", function($scope, $http) {
 
-    $scope.books = [{title: "Effective Java"},
-                    {title: "Java Concurrency in Practice"},
-                    {title: "Java Performance the Definitive Guide"},
-                    {title: "Clean Code"}];
+    $scope.booksLoaded = false;
+    $scope.books = [];
+
+    $scope.loadBooks = function() {
+        $http.get("https://gist.githubusercontent.com/marpiec/90c988b3a18a96d80215/raw/c67284bc5f72b48a3812f86b11415c01642d6f32/BestJavaBooks.json").success(function(data, status, headers) {
+            $scope.books = data;
+            $scope.booksLoaded = true;
+        }).error(function (data, status, headers, config) {
+            alert("Error communication with server!")
+        });
+    }
 
 });
